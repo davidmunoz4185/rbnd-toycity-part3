f0d39e9d0426dc5320a18e6e0c62a9968e94a343
davidmunoz4185 <david.munoz4185@gmail.com>
2016-07-13 16:05:28 +0200

    Product class ended

--- lib/product.rb
+++ lib/product.rb
@@ -1,11 +1,43 @@
 class Product
+  attr_reader :title, :price, :stock
+
   @@products = []
 
   def initialize(options={})
-    @@products << self
+    @title = options[:title]
+    @price = options[:title]
+    @stock = options[:stock]
+    add_to_products
   end
 
   def self.all
     @@products
   end
+
+  def self.find_by_title(title)
+    @@products.find {|product| product.title == title}
+  end
+
+  def self.in_stock
+    @@products.find_all {|product| product.in_stock?}
+  end
+
+  def in_stock?
+    @stock > 0
+  end
+
+  def purchase
+    @stock -= 1
+  end
+
+  private
+
+  def add_to_products
+    if (@@products.any? {|product| product.title == title})
+      raise DuplicateProductError, "'#{title}' already exists."
+    else
+      @@products << self
+    end
+ end
+
 end
